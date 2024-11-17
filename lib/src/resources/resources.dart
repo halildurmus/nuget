8f3a49aac951f365cb0215c70936d2c21f7573cd
Halil Durmus <durmushalil@proton.me>
2024-11-17 13:52:01 +0300

    feat: add support for retrieving report abuse URLs for packages
    Introduces functionality to construct URLs for reporting abuse for packages.

--- lib/src/resources/resources.dart
+++ lib/src/resources/resources.dart
@@ -1,6 +1,7 @@
 export 'autocomplete/autocomplete.dart';
 export 'package_content/package_content.dart';
 export 'package_metadata/package_metadata.dart';
+export 'report_abuse/report_abuse.dart';
 export 'resource.dart';
 export 'search/search.dart';
 export 'service_index/service_index.dart';
